e13a683c0f70d188a2a1e81bef7b069837d6003c
VadanaDaria <vadanaiulia20@stud.ase.ro>
2024-05-04 18:08:41 +0300

    csr ssr

---
+++ pages/ssr.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Ssr= ({data}) => {
+
+    return (
+        <div>
+            Hello, {data?.name?.title} {data?.name?.last} {data?.name?.first}
+        </div>
+    )
+}
+export default Ssr
+
+export async function getServerSidePops() {
+    
+    const response = await fetch("https://randomuser.me/api/?nat=us&randomapi");
+        const data = await response.json();
+
+
+    return {
+        pops:{
+
+        data: data.results[0]
+        }
+
+    }
+}
